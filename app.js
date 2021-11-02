788d9b041590f150b3dd5aa4838de51e1ef76853
BodaNabeel <needtoexplore01@gmail.com>
2021-11-02 16:12:07 +0530

    Movies showing in UI

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const inputField = document.querySelector(".input-field");
 const markupWrapper = document.querySelector(".markup-wrapper");
 const errorMessage = document.querySelector(".error-message");
 const titleMovie = document.querySelector("#title-movie");
+const containerArea = document.querySelector(".container-display-area");
 const apiKey = "b082a0d9";
-
+let markup;
 // Functions
 
 // Fetchig overview-API
 async function overview_fetchAPI(e) {
   const res = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s="${e}""`);
   const json = await res.json();
-  console.log(json);
+  updateContainer(json);
 }
 
-//
 // Checking if keyword value is empty or not
 const valueChecker = function (keyword) {
   if (keyword === "") {
@@ -23,10 +23,28 @@ const valueChecker = function (keyword) {
       errorMessage.style.display = "none";
     }, 1000);
   } else {
-    // TODO: Add the code for fetching API here
     overview_fetchAPI(keyword);
     titleMovie.innerHTML = keyword;
-    inputField.value = ""
+    inputField.value = "";
+    containerArea.innerHTML = "";
+  }
+};
+
+// Changing DOM for overview-review
+const updateContainer = function (data) {
+  for (i = 0; i < data.Search.length; i++) {
+    markup = `<div class="card">
+    <img
+      src="${data.Search[i].Poster}"
+      alt="Thumbnail of movie"
+      class="card-img"
+    />
+    <p class="card-name">${data.Search[i].Title}</p>
+    <button class="card-btn btn" id="btn${i}">
+      More Detail <i class="fas fa-chevron-right"></i>
+    </button>
+    </div>`;
+    containerArea.innerHTML += markup;
   }
 };
 
