c7d3c2dec128bf02e3887b24e398912e70bef673
BodaNabeel <needtoexplore01@gmail.com>
2021-11-02 13:14:31 +0530

    JS: overview-api fetched, movie-title made dynamic

--- app.js
+++ app.js
@@ -2,10 +2,20 @@
 const inputField = document.querySelector(".input-field");
 const markupWrapper = document.querySelector(".markup-wrapper");
 const errorMessage = document.querySelector(".error-message");
+const titleMovie = document.querySelector("#title-movie");
+const apiKey = "b082a0d9";
 
 // Functions
 
-// 1. Checking if keyword value is empty or not
+// Fetchig overview-API
+async function overview_fetchAPI(e) {
+  const res = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s="${e}""`);
+  const json = await res.json();
+  console.log(json);
+}
+
+//
+// Checking if keyword value is empty or not
 const valueChecker = function (keyword) {
   if (keyword === "") {
     errorMessage.style.display = "block";
@@ -14,6 +24,9 @@ const valueChecker = function (keyword) {
     }, 1000);
   } else {
     // TODO: Add the code for fetching API here
+    overview_fetchAPI(keyword);
+    titleMovie.innerHTML = keyword;
+    inputField.value = ""
   }
 };
 
