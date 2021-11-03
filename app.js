32f5268cc18315381ed97d52a7cf5d3cb6a5a0e2
BodaNabeel <needtoexplore01@gmail.com>
2021-11-03 12:14:06 +0530

    Displaying content on UI

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const markupWrapper = document.querySelector(".markup-wrapper");
 const errorMessage = document.querySelector(".error-message");
 const titleMovie = document.querySelector("#title-movie");
 const containerOverviewArea = document.querySelector(".container-display-area");
-
+const containerDetailArea = document.querySelector(".container-detail");
 const apiKey = "b082a0d9";
 
 let markup;
@@ -22,7 +22,7 @@ async function overview_fetchAPI(e) {
 async function detail_fetchAPI(e) {
   const res = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t="${e}"`);
   const json = await res.json();
-  console.log(json);
+  updateDetailContainer(json);
 }
 
 // getting title of card
@@ -76,30 +76,28 @@ const updateOverviewContainer = function (data) {
 // Changing DOM for detail-review
 const updateDetailContainer = function (data) {
   markup = `<img
-  src="movie-thumbnail.jpg"
+  src="${data.Poster}"
   alt="Thumbnail of movie"
   class="container-detail__img"
 />
 <aside class="container-detail__info">
-  <h2 class="container-detail__heading">Naruto</h2>
-  <p class="movie-info__des">Genre: X</p>
-  <p class="movie-info__des">Released: X</p>
-  <p class="movie-info__des">Rated: X</p>
-  <p class="movie-info__des">Rating: X</p>
-  <p class="movie-info__des">Null: X</p>
+  <h2 class="container-detail__heading">${data.Title}</h2>
+  <p class="movie-info__des">Genre: ${data.Genre}</p>
+  <p class="movie-info__des">Released: ${data.Released}</p>
+  <p class="movie-info__des">Writer: ${data.Writer}</p>
+  <p class="movie-info__des">Rating: ${data.Ratings[0].Value}</p>
+  <p class="movie-info__des">Rated: ${data.Rated}</p>
 </aside>
 <div class="container-detail__plot">
   <h2 class="plot__heading">Plot:</h2>
-  <p class="plot_para">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-    Molestias soluta blanditiis repellendus quam explicabo repudiandae
-    error eaque rerum deserunt nihil?
-  </p>
+  <p class="plot_para">${data.Plot}</p>
   <div class="buttons">
-    <button class="plot-btn imbd-btn">IMBD</button>
+    <button class="plot-btn imbd-btn"> <a class="imbd-link" target="_blank" rel="noopener noreferrer" href="https://www.imdb.com/title/${data.imdbID}/?ref_=fn_al_tt_1">IMBd</a></button>
     <button class="plot-btn default-btn">Close</button>
   </div>
 </div>`;
+  containerDetailArea.innerHTML = markup;
+  containerDetailArea.style.display = "grid";
 };
 inputField.addEventListener("keyup", function (e) {
   if (e.keyCode === 13) {
